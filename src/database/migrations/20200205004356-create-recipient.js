5452103ec6bff9e4876b806680d0c04ff5f9fbf5
Rafael Korz <rafael.korz@gmail.com>
2020-02-04 22:59:28 -0200

    start projeto

---
+++ src/database/migrations/20200205004356-create-recipient.js
@@ -0,0 +1,53 @@
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('recipient', {
+      id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      street: {
+        type: Sequelize.STRING,
+        allowNull: true
+      },
+      complement: {
+        type: Sequelize.STRING,
+        allowNull: true
+      },
+      state: {
+        type: Sequelize.STRING,
+        allowNull: true
+      },
+      city: {
+        type: Sequelize.STRING,
+        allowNull: true
+      },
+      zip_code: {
+        type: Sequelize.STRING,
+        allowNull: true
+      },
+      provider: {
+        type: Sequelize.BOOLEAN,
+        defaultValue: false,
+        allowNull: false
+      },
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+        allowNull: false
+      }
+    });
+  },
+
+  down: queryInterface => {
+    return queryInterface.dropTable('recipient');
+  }
+};
