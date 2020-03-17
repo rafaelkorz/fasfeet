126e49d6cdb448f7666f586c37338c5cf395c45f
Rafael Korz <rafael.korz@gmail.com>
2020-03-16 21:05:32 -0300

    Adicionado CORS

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ import 'dotenv/config';
 import express from 'express';
 import path from 'path';
 import * as Sentry from '@sentry/node';
+import cors from 'cors';
 import routes from './routes';
 import sentryConfig from './config/sentry';
 
@@ -21,6 +22,7 @@ class App {
   middlewares() {
     this.server.use(Sentry.Handlers.requestHandler());
     this.server.use(express.json());
+    this.server.use(cors());
     this.server.use(
       '/files',
       express.static(path.resolve(__dirname, '..', 'tmp', 'uploads'))
